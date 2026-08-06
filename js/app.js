5a73f589178feddea9f2fefce54e5338a68b80e1
ineocases <zumagamingarg@gmail.com>
2026-08-06 14:02:04 -0300

    Create app.js

---
+++ js/app.js
@@ -0,0 +1,12 @@
+import Home from "../screens/home.js";
+import {render} from "./router.js";
+
+render(Home());
+
+document
+.getElementById("newGame")
+.onclick=()=>{
+
+    alert("En el próximo paso haremos Crear Canal 🚀");
+
+};
