2cd56af5778aa55672865f7c43e6aba8bed1d95f
ineocases <zumagamingarg@gmail.com>
2026-08-06 23:18:06 -0300

    Create argentina.js

---
+++ data/creators/argentina.js
@@ -0,0 +1,51 @@
+export default [
+
+{
+    id:"davo",
+
+    name:"Davoo Xeneize",
+
+    followers:2500000,
+
+    niche:"Fútbol",
+
+    rarity:"rare",
+
+    collaboration:20,
+
+    reactions:30
+},
+
+{
+    id:"spreen",
+
+    name:"Spreen",
+
+    followers:12000000,
+
+    niche:"Gaming",
+
+    rarity:"legendary",
+
+    collaboration:8,
+
+    reactions:15
+},
+
+{
+    id:"coscu",
+
+    name:"Coscu",
+
+    followers:9000000,
+
+    niche:"Variedad",
+
+    rarity:"legendary",
+
+    collaboration:5,
+
+    reactions:20
+}
+
+];
