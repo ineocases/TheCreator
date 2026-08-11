1e4554bc91315cddf186dd0aeab7cbe6df2e2fbe
ineocases <zumagamingarg@gmail.com>
2026-08-11 10:58:26 -0300

    Delete ElCreadorrrr directory

--- ElCreadorrrr/app.js
+++
@@ -1,7 +0,0 @@
-// app.js - Entry point principal de El Creador
-
-import { initRouter } from './router.js';
-
-document.addEventListener('DOMContentLoaded', () => {
-    initRouter();
-});
--- ElCreadorrrr/js/router.js
+++
@@ -1,2 +0,0 @@
-// Compatibility entry point. The live app uses ../router.js.
-export * from "../router.js";
